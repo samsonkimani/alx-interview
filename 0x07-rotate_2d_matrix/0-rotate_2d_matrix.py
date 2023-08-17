6b58d8daaa4d992ab474fd9efeb5f3f376965808
samsonkimani <samsonkimani43@gmail.com>
2023-08-17 16:39:15 +0300

    rotating a 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" rotating a 2d array 90 degrees"""
+
+
+def rotate_2d_matrix(matrix):
+    """ rotating a 2d matrix"""
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for row in matrix:
+        row.reverse()
+
+    return matrix
