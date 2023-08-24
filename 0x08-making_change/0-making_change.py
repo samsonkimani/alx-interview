a75f49a2f4bd1ede599ac07551c63405ba56c6fc
samsonkimani <samsonkimani43@gmail.com>
2023-08-24 19:16:51 +0300

    trying to make the program more efficient

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,13 +7,13 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
     dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    dp[0] = 0  # Base case: no coins needed for total of 0
 
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
     if dp[total] == float('inf'):
-        return -1
+        return -1  # Total cannot be met by any number of coins
     else:
         return dp[total]
